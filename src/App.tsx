7329563c8af8021ae939db962ce0e84aed076562
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-04 16:26:24 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import LandingPage from "./pages/landing-page";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
