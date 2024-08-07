be01f0f4f151d34d5db87157be114ead06550bc0
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-07 22:24:16 +0300

    Update App component to include Header and under construction component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
-import LandingPage from "./pages/landing-page";
+import Header from "./components/Header";
+import LandingPage from "./components/under-construction";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<LandingPage />} />
         </Routes>
