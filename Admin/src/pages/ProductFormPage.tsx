ad60737242c4f85500e4172b634f75bb122cbc69
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-08 17:06:49 +0300

    Add user authentication check to ProductFormPage

--- Admin/src/pages/ProductFormPage.tsx
+++ Admin/src/pages/ProductFormPage.tsx
@@ -1,7 +1,15 @@
 import ProductForm from "@/components/ProductForm";
-import React from "react";
+import { useUser } from "@/hooks/useUser";
+import { useNavigate } from "react-router-dom";
 
 const ProductFormPage = () => {
+  const { user } = useUser();
+
+  const navigate = useNavigate();
+
+  if (!user) {
+    navigate("/");
+  }
   return (
     <main className="flex min-h-screen items-center justify-center bg-gray-100 py-20 px-4">
       <ProductForm />
