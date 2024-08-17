1ae5e820ac0015c91c7577a7f141d7bfa0703ef8
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-18 00:35:55 +0300

    chore: Update under-construction component for maintenance message

--- client/src/components/under-construction.tsx
+++ client/src/components/under-construction.tsx
@@ -4,22 +4,21 @@ const LandingPage = () => {
   return (
     <div className="flex min-h-[100dvh] flex-col items-center justify-center bg-white px-4 py-12 sm:px-6 lg:px-8">
       <div className="flex justify-center">
-        <Link to="#" >
+        <Link to="#">
           <div className=" rounded-full w-44 h-20 flex items-center justify-center">
-            <img className="h-20 w-44 text-primary" src="/logo.png" />
+            <img className=" w-28 text-primary" src="/logo-2.png" />
           </div>
         </Link>
       </div>
-      <div className="mx-auto max-w-md text-center">
-        <div className="inline-block rounded-lg bg-gray-200 px-3 py-1 text-sm font-medium">
-          Website Under Construction
-        </div>
-        <h1 className="mt-4 text-3xl font-bold tracking-tight text-foreground sm:text-4xl">
-          Coming Soon
+      <div className="mx-auto max-w-md text-center flex flex-col">
+        <h1 className="mt-4 text-xl tracking-tight text-gray-500 sm:text-xl">
+          Offline for maintenance
         </h1>
         <p className="mt-4 text-muted-foreground">
-          We're hard at work building an amazing new website for you. Stay tuned
-          for updates!
+          This app is undergoing maintenance right now.
+        </p>
+        <p className="text-muted-foreground">
+          We will be back shortly. Thank you for your patience.
         </p>
         <div className="mt-6">
           <div className="w-full bg-gray-200 rounded-full h-2.5 mb-4">
