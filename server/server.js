02b09b0a941419a4fd9cde75f1efd8d56c2c4fcb
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-18 02:52:53 +0300

    Update server configuration and add user routes

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
 import express from "express";
+import userRouter from "./routes/user-routes.js";
+import ErrorHandler from "./middlewares/error-handler.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/auth", userRouter);
+app.use(ErrorHandler);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
