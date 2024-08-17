af95d526ed3fadcd2b77f1151746c5380cc6bfb6
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-18 02:11:20 +0300

    Update project configuration files

---
+++ server/config/config.js
@@ -0,0 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+// export const DATABASE_URL = process.env.
+// DATABASE_URL
+// console.log(DATABASE_URL);
+// export const PORT = process.env.PORT || 3000;
+export const NODE_ENV = process.env.NODE_ENV;
