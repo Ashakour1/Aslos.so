b4abc197a8a21c1e39c4887309321854b0dc31e7
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-20 03:01:47 +0300

    product model and update config file and add upload

--- server/config/config.js
+++ server/config/config.js
@@ -6,3 +6,6 @@ dotenv.config();
 // console.log(DATABASE_URL);
 // export const PORT = process.env.PORT || 3000;
 export const NODE_ENV = process.env.NODE_ENV;
+export const CLOUDINARY_CLOUDNAME = process.env.CLOUDINARY_CLOUDNAME;
+export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
+export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
