e4189ede0a291b6bd38dc2216298a3ea296201c2
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-24 19:55:51 +0300

    Add JWT secret to configuration file

--- server/config/config.js
+++ server/config/config.js
@@ -9,3 +9,4 @@ export const NODE_ENV = process.env.NODE_ENV;
 export const CLOUDINARY_CLOUDNAME = process.env.CLOUDINARY_CLOUDNAME;
 export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
 export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
+export const JWT_SECRET = process.env.JWT_SECRET;
