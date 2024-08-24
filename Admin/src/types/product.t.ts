0532594bf418bb928ef0056674912fc09a1e7525
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-24 19:56:57 +0300

    Add product type definitions

---
+++ Admin/src/types/product.t.ts
@@ -0,0 +1,18 @@
+export type formData = {
+  name: string;
+  description: string;
+  category: string;
+  price: Number;
+  stock: Number;
+  image: File | string;
+};
+
+export type Products = {
+  id: string;
+  name: string;
+  description: string;
+  category: string;
+  price: Number;
+  stock: Number;
+  image: string;
+};
