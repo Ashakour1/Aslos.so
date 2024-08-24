bbd126d81b03e85b9c65e11065eb935d8da7ff09
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-24 19:56:15 +0300

    Add product routes for CRUD operations

--- server/server.js
+++ server/server.js
@@ -2,16 +2,23 @@ import express from "express";
 import userRouter from "./routes/user-routes.js";
 import ErrorHandler from "./middlewares/error-handler.js";
 import dotenv from "dotenv";
+import productRouter from "./routes/product-routes.js";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
-
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", userRouter);
+app.use("/api/products", productRouter);
 app.use(ErrorHandler);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
