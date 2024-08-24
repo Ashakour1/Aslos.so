273b4d785c45268201e2851242ad2ea6844817be
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-24 19:57:16 +0300

    Add ProductPage component for managing products

---
+++ Admin/src/pages/ProductPage.tsx
@@ -0,0 +1,41 @@
+import { ProductTable } from "@/components/ProductTable";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Table,
+  TableHeader,
+  TableRow,
+  TableHead,
+  TableBody,
+  TableCell,
+} from "@/components/ui/table";
+import { MoveHorizontalIcon } from "lucide-react";
+import { Link } from "react-router-dom";
+const ProductPage = () => {
+  return (
+    <div className="max-w-[1200px] mx-auto my-10">
+      <div className="flex justify-between my-5 items-center">
+        <div className="flex flex-col gap-2">
+          <h1 className="text-3xl font-semibold">Products</h1>
+          <p className="text-gray-700">
+            Manage your products and view your inventory
+          </p>
+        </div>
+        <div>
+          <Link to="/dashboard/products/add">
+            <Button>Create New Product</Button>
+          </Link>
+        </div>
+      </div>
+      <ProductTable />
+    </div>
+  );
+};
+
+export default ProductPage;
