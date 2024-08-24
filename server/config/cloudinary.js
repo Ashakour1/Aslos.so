8be9a1b1915201c675ddbcaa1ff16d7622e44254
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-24 19:56:04 +0300

    Update cloudinary configuration for better readability and consistency

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,7 +1,16 @@
-import cloudinary from 'cloudinary'
+import cloudinary from "cloudinary";
 
-import { CLOUDINARY_CLOUDNAME,CLOUDINARY_API_KEY,CLOUDINARY_API_SECRET } from './config'
+
+import {
+  CLOUDINARY_CLOUDNAME,
+  CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET,
+} from "./config.js";
 
 cloudinary.v2.config({
-    
-})
+  cloud_name: CLOUDINARY_CLOUDNAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
+});
+
+export default cloudinary.v2;
