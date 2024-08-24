d775193e32c06a40de310300fec405c784813f82
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-24 19:57:26 +0300

    Add ProductFormPage component for managing product form

---
+++ Admin/src/pages/ProductFormPage.tsx
@@ -0,0 +1,12 @@
+import ProductForm from "@/components/ProductForm";
+import React from "react";
+
+const ProductFormPage = () => {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gray-100 py-20 px-4">
+      <ProductForm />
+    </main>
+  );
+};
+
+export default ProductFormPage;
