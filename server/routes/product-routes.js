5c1f35725b37eb11b111f3c382478393570e48a8
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-24 19:53:42 +0300

    Add product routes for CRUD operations

---
+++ server/routes/product-routes.js
@@ -0,0 +1,18 @@
+import express from "express";
+import upload from "../middlewares/upload-cloudinary.js";
+import {
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  getProducts,
+  getProductById,
+} from "../controllers/product-controller.js";
+const productRouter = express.Router();
+
+productRouter.get("/", getProducts);
+productRouter.get("/:id", getProductById);
+productRouter.post("/create", upload.single("image"), createProduct);
+productRouter.put("/update/:id", upload.single("image"), updateProduct);
+productRouter.delete("/delete/:id", deleteProduct);
+
+export default productRouter;
