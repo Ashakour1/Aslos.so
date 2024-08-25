3edb9e375a5e8d2b0579bd240b71d10c0d66afc6
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-25 16:15:03 +0300

    Add OrdersPage component for managing orders and inventory

---
+++ Admin/src/pages/OrdersPage.tsx
@@ -0,0 +1,25 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+import { Link } from "react-router-dom";
+
+const OrdersPage = () => {
+  return (
+    <main className="max-w-[1200px] mx-auto my-10">
+      <div className="flex justify-between my-5 items-center">
+        <div className="flex flex-col gap-2">
+          <h1 className="text-3xl font-semibold">Orders</h1>
+          <p className="text-gray-700">
+            Manage your orders and view your inventory
+          </p>
+        </div>
+        <div>
+          <Link to="/dashboard/orders/add">
+            <Button>Create New Order</Button>
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default OrdersPage;
