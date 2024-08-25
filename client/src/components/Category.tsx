de327ede27fe645c5887d1a180b205c89137851e
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-25 21:30:59 +0300

    Add Category component for displaying category information

---
+++ client/src/components/Category.tsx
@@ -0,0 +1,23 @@
+import { type Category } from "../types/category.t";
+
+type CategoryProps = {
+  category: Category;
+};
+const Category = ({ category }: CategoryProps) => {
+  return (
+    <div>
+      <div className="relative my-2">
+        <img
+          src={category.image}
+          className="absolute inset-0 h-full object-cover w-full"
+          alt=""
+        />
+        <div className="relative inset-0 py-60  bg-black bg-opacity-50 flex items-center justify-center">
+          <h1 className="text-white text-3xl font-semibold">{category.name}</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Category;
