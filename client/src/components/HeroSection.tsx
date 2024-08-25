4ab9770a3493dcc52531c1642ab4cfd3d1c236c1
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-25 19:39:55 +0300

    Add HeroSection component for landing page

---
+++ client/src/components/HeroSection.tsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+
+const HeroSection = () => {
+  return (
+    <section className="relative w-full h-screen overflow-hidden">
+      {/* Desktop Image */}
+      <img
+        src="/landing.png"
+        alt="desktop view"
+        className="hidden md:block absolute inset-0 w-full h-full object-cover md:object-center lg:object-fill"
+      />
+      {/* Mobile Image */}
+      <img
+        src="/landing-mobile.png"
+        alt="mobile view"
+        className="block md:hidden absolute inset-0 object-cover w-full h-full"
+      />
+      <div className="relative z-10 flex items-center justify-center w-full h-full bg-gradient-to-t  to-transparent">
+        <div className="text-center text-white max-w-lg mx-auto px-4 py-8 md:px-8 md:py-12">
+          <div className="mt-36 md:mt-0">
+            <h1 className="text-2xl md:text-4xl font-bold mb-2">
+              FALL '24 COLLECTION
+            </h1>
+            <p className=" text-base md:text-xl mb-2 ">
+              PUSH THE BODY, FREE THE MIND
+            </p>
+            <Link to="/shop">
+              <button className="bg-white text-black px-8 py-2 font-medium hover:bg-gray-200 transition duration-300">
+                SHOP NOW
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
