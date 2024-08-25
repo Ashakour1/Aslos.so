014b20bd2659aa26fed0211709f9a74c82808d8f
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-25 16:14:54 +0300

    Add LoginPage component for user login functionality

---
+++ Admin/src/pages/LoginPage.tsx
@@ -0,0 +1,11 @@
+import LoginForm from "@/components/LoginForm";
+
+const LoginPage = () => {
+  return (
+    <div className="max-w-md flex justify-center items-center mx-auto h-screen ">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
