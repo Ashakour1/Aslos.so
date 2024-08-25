4caa7e63a4d89b56c2b2d4585d850c72b66af6b5
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-25 16:12:16 +0300

    Refactor routing in App component for better organization and readability

--- Admin/src/App.tsx
+++ Admin/src/App.tsx
@@ -1,23 +1,74 @@
+import { Toaster } from "react-hot-toast";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/nav/Header.tsx";
-import Dashboard from "./pages/Dashboard.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
 import ProductFormPage from "./pages/ProductFormPage.tsx";
 import ProductPage from "./pages/ProductPage.tsx";
-import { Toaster } from "react-hot-toast";
+
+import { PropsWithChildren } from "react";
+import Dashboard from "./pages/Dashboard.tsx";
+import OrdersPage from "./pages/OrdersPage.tsx";
+
+const MainLayout = ({ children }: PropsWithChildren<{}>) => {
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
+};
 
 function App() {
   return (
     <>
       <Router>
         <Toaster />
-        <Header />
+
         <Routes>
-          <Route path="/dashboard/products/add" element={<ProductFormPage />} />
-          <Route path="/dashboard/product/update/:id" element={<ProductFormPage />} />
-          
-          <Route path="/Dashboard" element={<Dashboard />} />
-          <Route path="/dashboard/products" element={<ProductPage />} />
+          <Route path="/" element={<LoginPage />} />
+          <Route
+            path="/dashboard/products/add"
+            element={
+              <MainLayout>
+                <ProductFormPage />
+              </MainLayout>
+            }
+          />
+          <Route
+            path="/dashboard/product/update/:id"
+            element={
+              <MainLayout>
+                <ProductFormPage />
+              </MainLayout>
+            }
+          />
+
+          <Route
+            path="/dashboard/products"
+            element={
+              <MainLayout>
+                <ProductPage />
+              </MainLayout>
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <MainLayout>
+                <Dashboard />
+              </MainLayout>
+            }
+          />
+          <Route path="*" element={<div>404</div>} />
+          <Route
+            path="/dashboard/orders"
+            element={
+              <MainLayout>
+                <OrdersPage />
+              </MainLayout>
+            }
+          />
         </Routes>
       </Router>
     </>
