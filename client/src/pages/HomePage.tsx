bceb028f3c64a5de54fab60465425f3eb18cc5a6
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-25 19:40:21 +0300

    Add HomePage component

---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import HeroSection from "@/components/HeroSection";
+
+const HomePage = () => {
+  return (
+    <div className="w-full h-full">
+      <HeroSection />
+    </div>
+  );
+};
+
+export default HomePage;
