02f961e4479694ee422fb9002e75afdff0d692d4
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-25 21:32:08 +0300

    Add CategoriesSection component and data for displaying categories

---
+++ client/src/data/categories-data.tsx
@@ -0,0 +1,16 @@
+import { Category } from "@/types/category.t";
+
+export const Categories: Category[] = [
+  {
+    name: "Run",
+    image: "/landing-mobile.png",
+  },
+  {
+    name: "Recap",
+    image: "/landing-mobile.png",
+  },
+  {
+    name: "Train",
+    image: "/landing-mobile.png",
+  },
+];
