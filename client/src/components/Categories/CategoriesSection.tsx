707cbc9c230ca377d46d61496dd1db17e381ef37
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-25 22:41:24 +0300

    Update max-width and font-weight in CategoriesSection component

--- client/src/components/Categories/CategoriesSection.tsx
+++ client/src/components/Categories/CategoriesSection.tsx
@@ -3,9 +3,9 @@ import Category from "./Category";
 
 const CategorySection = () => {
   return (
-    <main className="max-w-[1140px] mx-auto md:px-0 px-4">
+    <main className="max-w-[1180px] mx-auto md:px-0 px-4">
       <div className="py-4">
-        <h1 className="text-2xl font-bold py-4">Shop By Category</h1>
+        <h1 className="text-2xl font-semibold py-4">Shop By Category</h1>
         <hr className="border-1" />
         <div className="grid grid-cols-1 md:grid-cols-3 py-4 gap-4">
           {Categories.map((item, index) => (
