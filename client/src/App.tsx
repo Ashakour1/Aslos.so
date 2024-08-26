5c36641a8980285bf5973c3e83e5450b86a9bab9
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-26 17:08:18 +0300

    Update landing page routing and components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,17 @@
 import "./App.css";
+import Header from "./components/Header";
 // import Header from "./components/Header";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 import LandingPage from "./components/under-construction";
 
 function App() {
   return (
     <>
       <Router>
-        {/* <Header /> */}
+        <Header />
         <Routes>
-          <Route path="/" element={<LandingPage />} />
+          <Route path="/" element={<HomePage />} />
         </Routes>
       </Router>
     </>
