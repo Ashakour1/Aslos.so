ad759157fa1aef2ef410ab39f22d7d0b9eac5a20
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-26 22:28:13 +0300

    Update CategoriesSection component styling and heading text

--- client/src/components/Categories/CategoriesSection.tsx
+++ client/src/components/Categories/CategoriesSection.tsx
@@ -3,9 +3,9 @@ import Category from "./Category";
 
 const CategorySection = () => {
   return (
-    <main className="max-w-[1180px] mx-auto md:px-0 px-4">
+    <main className="max-w-[1180px] mx-auto md:px-4 lg:px-0 px-4">
       <div className="py-4">
-        <h1 className="text-2xl font-semibold py-4">Shop By Category</h1>
+        <h1 className="text-2xl font-semibold py-4">SHOP BY CATEGORY</h1>
         <hr className="border-1" />
         <div className="grid grid-cols-1 md:grid-cols-3 py-4 gap-4">
           {Categories.map((item, index) => (
