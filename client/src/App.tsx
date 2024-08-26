92955d9c08bf01cd09768e6fb6b052ee948e5938
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-26 22:28:42 +0300

    Add proxy configuration for API requests to /api/products

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,17 @@
 import "./App.css";
-import Header from "./components/Header";
+// import Header from "./components/Header";
 // import Header from "./components/Header";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
 import NotFound from "./components/NotFound";
+import LandingPage from "./components/under-construction";
 
 function App() {
   return (
     <>
       <Router>
-        <Header />
+        {/* <Header /> */}
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<LandingPage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
