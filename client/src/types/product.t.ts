cf9cc7211f076b7ecbb5be03e3d6120c0431a5a1
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-26 22:25:35 +0300

    Add ProductType interface for product data

---
+++ client/src/types/product.t.ts
@@ -0,0 +1,10 @@
+export type ProductType = {
+  name: string;
+  description: string;
+  category: string;
+  price: number;
+  stock: number;
+  image: string;
+  createdAt?: string;
+  updatedAt?: string;
+};
