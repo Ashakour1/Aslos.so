af01bed21d0eaf8c2985d65a1ceec15e4bc4f947
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-26 14:21:20 +0300

    Move Category and CategoriesSection components to separate directory

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,6 @@
 import "./App.css";
-import Header from "./components/Header";
 // import Header from "./components/Header";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
 import LandingPage from "./components/under-construction";
 
 function App() {
