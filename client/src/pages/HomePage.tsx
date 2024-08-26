e374789ff4152599f72152bdcda2af6db6ea0e69
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-26 22:28:29 +0300

    Add Products component to display products on the landing page

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import CategorySection from "@/components/Categories/CategoriesSection";
 import HeroSection from "@/components/HeroSection";
+import Products from "@/components/Products/products";
 
 const HomePage = () => {
   return (
     <div className="w-full h-full">
       <HeroSection />
       <CategorySection />
+      <Products />
     </div>
   );
 };
