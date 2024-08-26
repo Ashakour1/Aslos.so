de63f5054acc8ec1a1e6302bc4a3400dc72d0b2a
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-26 17:14:09 +0300

    Update NotFound component to display a more user-friendly 404 page

--- client/src/components/NotFound.tsx
+++ client/src/components/NotFound.tsx
@@ -1,7 +1,7 @@
 const NotFound = () => {
   return (
-    <div className="flex flex-col justify-center items-center text-center min-h-screen">
-      <h1>404 Not Found</h1>
+    <div className="flex flex-col justify-center mt-52 text-center">
+      <h1>404 | Not Found</h1>
       <p>Sorry, the page you are looking for does not exist.</p>
     </div>
   );
