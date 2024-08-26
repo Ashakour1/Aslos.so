fc14c68693ccdd5a20d62adc36439af2c5fa2979
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-26 22:26:43 +0300

    Add Product component for displaying individual products

---
+++ client/src/components/Products/product.tsx
@@ -0,0 +1,19 @@
+import { type ProductType } from "@/types/product.t";
+
+type ProductProps = {
+  product: ProductType;
+};
+
+const Product = ({ product }: ProductProps) => {
+  return (
+    <div className="flex flex-col bg-white overflow-hidden">
+      <img src={product.image} alt="Product" className="object-fill" />
+      <div className="flex flex-col gap-1">
+        <h1 className="text-base font-semibold ">{product.name}</h1>
+        <p className="text-gray-600">{product.price}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
