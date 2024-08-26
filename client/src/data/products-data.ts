f87582bbc4e5e95b429b1ffc04f9fe20ad68b32f
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-26 22:25:13 +0300

    Add products data to client/src/data directory

---
+++ client/src/data/products-data.ts
@@ -0,0 +1,43 @@
+import { ProductType } from "../types/product.t";
+export const products: ProductType[] = [
+  {
+    name: "Nike Air",
+    description:
+      "The Nike Air is a sneaker that needs no introduction. It's been a staple in the sneaker community for years and has been worn by everyone from athletes to celebrities.",
+    category: "Run",
+    price: 100,
+    stock: 10,
+    image:
+      "https://asrv.com/cdn/shop/files/BLACK_ASRV_EstablishedTee_c42f7fce-0293-4b1a-993d-c180b9ea2131_600x.jpg?v=1710701915",
+  },
+  {
+    name: "Adidas Boost",
+    description:
+      "The Adidas Boost is a sneaker that needs no introduction. It's been a staple in the sneaker community for years and has been worn by everyone from athletes to celebrities.",
+    category: "Recap",
+    price: 120,
+    stock: 15,
+    image:
+      "https://asrv.com/cdn/shop/products/ASRVE-Comm-425-Edit_600x.jpg?v=1673555835",
+  },
+  {
+    name: "New Balance",
+    description:
+      "The New Balance is a sneaker that needs no introduction. It's been a staple in the sneaker community for years and has been worn by everyone from athletes to celebrities.",
+    category: "Train",
+    price: 80,
+    stock: 5,
+    image:
+      "https://img.lazcdn.com/g/p/8c4ba43f51e014b168ef78746994ce79.jpg_960x960q80.jpg_.webp",
+  },
+  {
+    name: "Puma",
+    description:
+      "The Puma is a sneaker that needs no introduction. It's been a staple in the sneaker community for years and has been worn by everyone from athletes to celebrities.",
+    category: "Run",
+    price: 90,
+    stock: 20,
+    image:
+      "https://asrv.com/cdn/shop/products/ASRVE-Comm-425-Edit_600x.jpg?v=1673555835",
+  },
+];
