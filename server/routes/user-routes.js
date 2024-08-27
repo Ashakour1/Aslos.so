87a327e05376eb6c071d42961d783426c46ee3ec
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-27 22:31:31 +0300

    Add getUser route to retrieve user data

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { registerUser,loginUser } from "../controllers/user-controller.js";
+import {
+  registerUser,
+  loginUser,
+  getUser,
+} from "../controllers/user-controller.js";
+import { authMiddleware } from "../middlewares/auth-middleware.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
-
+userRouter.get("/:id", authMiddleware, getUser);
 
 export default userRouter;
