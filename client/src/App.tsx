0eed2bcbc59f49ace8a8a60d09c10c6bdccd3af5
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-27 23:34:26 +0300

    Add HomePage and Products pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,22 @@
 import "./App.css";
 // import Header from "./components/Header";
-// import Header from "./components/Header";
+import Header from "./components/Header";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import NotFound from "./components/NotFound";
 import LandingPage from "./components/under-construction";
+import HomePage from "./pages/HomePage";
+import Products from "./pages/Products";
 
 function App() {
   return (
     <>
       <Router>
-        {/* <Header /> */}
+        <Header />
         <Routes>
-          <Route path="/" element={<LandingPage />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="*" element={<NotFound />} />
+          <Route path="/shope/:sex" element={<Products />} />
+          <Route path="/shope/*" element={<NotFound />} />
         </Routes>
       </Router>
     </>
