f9a1f692c4d7b2cd70d7cb000525c3932bf4df68
Ashakuur1 <engshakrayare114@gmail.com>
2024-08-28 10:54:02 +0300

    Refactor import statements in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,8 @@
 import "./App.css";
 // import Header from "./components/Header";
-import Header from "./components/Header";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Header from "./components/Header";
 import NotFound from "./components/NotFound";
-import LandingPage from "./components/under-construction";
 import HomePage from "./pages/HomePage";
 import Products from "./pages/Products";
 
