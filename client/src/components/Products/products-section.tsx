f1ac9bcf448fa3f421baf3336d06bc534d83bb1b
Ashakuur1 <engshakrayare114@gmail.com>
2024-09-01 17:00:05 +0300

    Update Products page to fetch new products and handle additional URL parameters

--- client/src/components/Products/products-section.tsx
+++ client/src/components/Products/products-section.tsx
@@ -1,36 +1,31 @@
-import { products } from "@/data/products-data";
+// import { products } from "@/data/products-data";
 import Product from "./product";
-// import axios from "axios";
-// import { useEffect, useState } from "react";
-// import { ProductType } from "@/types/product.t";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { ProductType } from "@/types/product.t";
+import { Link } from "react-router-dom";
 const Products = () => {
-  //   const [products, setProducts] = useState<ProductType[]>([]);
+  const [products, setProducts] = useState<ProductType[]>([]);
 
-  //   const fetchProducts = async () => {
-  //     try {
-  //       const { data } = await axios.get("/api/products");
+  const fetchProducts = async () => {
+    try {
+      const { data } = await axios.get("/api/products?new=true");
 
-  //       const sortedProducts = data
-  //         .sort(
-  //           (a: any, b: any) =>
-  //             new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
-  //         )
-  //         .slice(0, 4);
-  //       setProducts(sortedProducts);
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   };
+      setProducts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  //   useEffect(() => {
-  //     fetchProducts();
-  //   }, []);
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
-  //   const getLatestProducts = (products: ProductType[]) => {
-  //     return products
-  //       .sort((a, b) => new Date(b.createdAt).getTime - new Date(a.created ?? ""))
-  //       .slice(0, 4);
-  //   };
+  // const getLatestProducts = (products: ProductType[]) => {
+  //   return products
+  //     .sort((a, b) => new Date(b.createdAt).getTime - new Date(a.created ?? ""))
+  //     .slice(0, 4);
+  // };
   return (
     <main className="max-w-[1180px] mx-auto md:px-4 lg:px-0 px-4 py-4">
       <div className="flex items-center justify-between ">
@@ -38,9 +33,11 @@ const Products = () => {
           <p className="font-medium text-sm">SHOP ALL PRODUCTS</p>
           <h1 className="text-2xl font-semibold">NEW IN</h1>
         </div>
-        <button className="border border-black px-4 py-2">
-          SHOP ALL PRODUCTS
-        </button>
+        <Link to="/shop" className="text-sm font-medium">
+          <button className="border border-black px-4 py-2">
+            SHOP ALL PRODUCTS
+          </button>
+        </Link>
       </div>
       <div className="grid lg:grid-cols-4   md:grid-cols-3 sm:grid-cols-1 grid-cols-1 gap-2 py-5">
         {products.map((item, index) => (
