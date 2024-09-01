417becaa860fa82f2dba9d9b0751d14bf549f0d0
Ashakuur1 <engshakrayare114@gmail.com>
2024-09-01 17:00:26 +0300

    Update Category component to include link to category page

--- client/src/components/Categories/Category.tsx
+++ client/src/components/Categories/Category.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { type Category } from "../../types/category.t";
 
 type CategoryProps = {
@@ -6,16 +7,18 @@ type CategoryProps = {
 const Category = ({ category }: CategoryProps) => {
   return (
     <div>
-      <div className="relative">
+      <Link to={`/shop/cat/${category.name}`} className="relative">
         <img
           src={category.image}
           className="absolute inset-0 h-full object-cover w-full"
           alt=""
         />
         <div className="relative inset-0 lg:py-60 md:py-40 py-60  bg-black bg-opacity-50 flex items-center justify-center">
-          <h1 className="text-white text-3xl font-semibold">{category.name}</h1>
+          <h1 className="text-white text-3xl font-semibold">
+            {category.name.toUpperCase()}
+          </h1>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
