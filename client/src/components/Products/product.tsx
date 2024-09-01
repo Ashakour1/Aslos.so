b8f9cba11a02926d303be9b00815d4a4beecab1a
Ashakuur1 <engshakrayare114@gmail.com>
2024-09-01 17:00:17 +0300

    Update Product component styling and structure

--- client/src/components/Products/product.tsx
+++ client/src/components/Products/product.tsx
@@ -6,15 +6,15 @@ type ProductProps = {
 
 const Product = ({ product }: ProductProps) => {
   return (
-    <div className="flex flex-col bg-white overflow-hidden">
+    <div className="flex flex-col bg-white shadow-sm overflow-hidden">
       <img
         src={product.image}
-        alt="Product"
-        className="w-full h-72  object-cover"
+        alt={product.name}
+        className="w-full h-72 object-cover"
       />
-      <div className="flex flex-col gap-1">
-        <h1 className="text-base font-semibold ">{product.name}</h1>
-        <p className="text-gray-600">{product.price}</p>
+      <div className="p-4 flex flex-col gap-2">
+        <h1 className="text-lg font-semibold">{product.name}</h1>
+        <p className="text-gray-600 text-sm">{product.price}</p>
       </div>
     </div>
   );
