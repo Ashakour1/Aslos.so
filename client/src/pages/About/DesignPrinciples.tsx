10b1d27be3dca57982b0a840c6006403dce42be6
Ashakuur1 <engshakrayare114@gmail.com>
2024-09-02 21:56:48 +0300

    Add new About page components

---
+++ client/src/pages/About/DesignPrinciples.tsx
@@ -0,0 +1,70 @@
+const DesignPrinciples = () => {
+  return (
+    <main className="w-full h-full">
+      <div className="relative flex flex-col items-center justify-center h-[500px]  overflow-hidden">
+        <img
+          src="/banner3.png"
+          alt=""
+          className="absolute w-full h-full p-4  inset-0 object-cover"
+        />
+        <div className="relative text-center  text-black max-w-4xl mx-auto px-4 py-8 md:px-8 md:pt-12 ">
+          <h1 className=" text-2xl md:text-7xl font-semibold">
+            Design Principles
+          </h1>
+        </div>
+      </div>
+      {/* Our Philosophy content
+       */}
+      <section className="max-w-4xl mx-auto py-5 text-gray-500 lg:px-0 md:px-4 px-5">
+        <div className="flex flex-col items-start justify-start py-8">
+          <p className="text-sm  ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+            laboriosam, voluptates, quidem, quas quos quia voluptatem
+            exercitationem quibusdam molestias quae doloremque. Quos, quas
+            voluptates. Quisquam, quae. Quibusdam, quos. Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Nemo laboriosam, voluptates,
+            quidem, quas quos quia voluptatem exercitationem quibusdam molestias
+            quae doloremque. Quos, quas voluptates. Quisquam, quae. Quibusdam,
+            quos.
+          </p>
+          <img src="/landing.png" alt="" className="w-full h-56 py-4" />
+          <p className="text-sm  ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+            laboriosam, voluptates, quidem, quas quos quia voluptatem
+            exercitationem quibusdam molestias quae doloremque. Quos, quas
+            voluptates. Quisquam, quae. Quibusdam, quos. Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Nemo laboriosam, voluptates,
+            quidem, quas quos quia voluptatem exercitationem quibusdam molestias
+            quae doloremque. Quos, quas voluptates. Quisquam, quae. Quibusdam,
+            quos.
+          </p>
+        </div>
+        <div className="flex flex-col items-start justify-start py-8">
+          <p className="text-sm  ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+            laboriosam, voluptates, quidem, quas quos quia voluptatem
+            exercitationem quibusdam molestias quae doloremque. Quos, quas
+            voluptates. Quisquam, quae. Quibusdam, quos. Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Nemo laboriosam, voluptates,
+            quidem, quas quos quia voluptatem exercitationem quibusdam molestias
+            quae doloremque. Quos, quas voluptates. Quisquam, quae. Quibusdam,
+            quos.
+          </p>
+          <img src="/landing.png" alt="" className="w-full h-56 py-4" />
+          <p className="text-sm  ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+            laboriosam, voluptates, quidem, quas quos quia voluptatem
+            exercitationem quibusdam molestias quae doloremque. Quos, quas
+            voluptates. Quisquam, quae. Quibusdam, quos. Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Nemo laboriosam, voluptates,
+            quidem, quas quos quia voluptatem exercitationem quibusdam molestias
+            quae doloremque. Quos, quas voluptates. Quisquam, quae. Quibusdam,
+            quos.
+          </p>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default DesignPrinciples;
