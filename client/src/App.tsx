41168b6853fb2f9e9cade914464a4390e23f20d3
Ashakuur1 <engshakrayare114@gmail.com>
2024-09-03 22:05:56 +0300

    Refactor routing paths in App component and update product type

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
 // import Header from "./components/Header";
+import LandingPage from "./components/under-construction";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Header from "./components/Header";
+// import Header from "./components/Header";
 import NotFound from "./components/NotFound";
-import HomePage from "./pages/HomePage";
+// import HomePage from "./pages/HomePage";
 import Products from "./pages/Products";
 import OurPhilosophy from "./pages/About/OurPhilosophy";
 import DesignPrinciples from "./pages/About/DesignPrinciples";
@@ -13,9 +14,9 @@ function App() {
   return (
     <>
       <Router>
-        <Header />
+        {/* <Header /> */}
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<LandingPage />} />
           <Route path="*" element={<NotFound />} />
           <Route path="/shop/sex/:sex" element={<Products />} />
           <Route path="/shop/cat/:category" element={<Products />} />
@@ -24,9 +25,11 @@ function App() {
           {/* <Route path="/shop/:id" element={<ProductDetail />} /> */}
           <Route path="/shop/*" element={<NotFound />} />
           <Route path="/about/our-philosophy" element={<OurPhilosophy />} />
-          <Route path="/about/design-principles" element={<DesignPrinciples />} />
+          <Route
+            path="/about/design-principles"
+            element={<DesignPrinciples />}
+          />
           <Route path="/about/*" element={<NotFound />} />
-
         </Routes>
       </Router>
     </>
