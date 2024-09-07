9217152e6b10cf1c8a1be9100f4c765a6271737d
Ashakuur1 <engshakrayare114@gmail.com>
2024-09-07 18:51:03 +0300

    Refactor CartItems component to use useCart hook and update UI

---
+++ client/src/components/CartItems.tsx
@@ -0,0 +1,112 @@
+import { useCart } from "@/features/useCart";
+import CartItem from "./CartItem";
+import { Link } from "react-router-dom";
+
+const CartItems = () => {
+  const { products, totalItems, totalPrice, tax, totalPriceWithTax } = useCart(
+    (state) => ({
+      products: state.products,
+      totalItems: state.totalItems,
+      totalPrice: state.totalPrice,
+      tax: state.tax,
+      totalPriceWithTax: state.totalPriceWithTax,
+    })
+  );
+
+  console.log(tax);
+  return (
+    <section>
+      {products.length === 0 ? (
+        <div className="text-center  flex flex-col mt-44 justify-center items-center">
+          <h2 className="text-2xl font-bold text-gray-900">
+            Your cart is empty
+          </h2>
+          <p className="mt-2">
+            Looks like you haven't added any items to the cart yet.
+          </p>
+          <Link to="/shop" className="text-sm font-medium">
+            <button className="border border-black px-4 py-2 my-4">
+              Back to Shop
+            </button>
+          </Link>
+        </div>
+      ) : (
+        <div className="mx-auto max-w-[1180px] px-4 py-2 sm:px-6 sm:py-4 lg:px-8">
+          <header className="text-center">
+            <h1 className="text-xl font-bold text-gray-900 sm:text-3xl">
+              Your Cart
+            </h1>
+          </header>
+
+          <div className="mt-8">
+            <div className="space-y-4">
+              {products.map((product, index) => (
+                <CartItem product={product} key={index} />
+              ))}
+            </div>
+
+            <div className="mt-8 flex justify-end border-t border-gray-100 pt-8">
+              <div className="w-screen max-w-lg space-y-4">
+                <dl className="space-y-0.5 text-sm text-gray-700">
+                  <div className="flex justify-between">
+                    <dt>Subtotal</dt>
+                    <dd>${totalPrice}</dd>
+                  </div>
+
+                  <div className="flex justify-between">
+                    <dt>VAT(Canshuur) </dt>
+                    <dd>${tax}</dd>
+                  </div>
+                  {/* 
+                <div className="flex justify-between">
+                  <dt>Discount</dt>
+                  <dd>-£20</dd>
+                </div> */}
+
+                  <div className="flex justify-between !text-base font-medium">
+                    <dt>Total</dt>
+                    <dd>${totalPriceWithTax}</dd>
+                  </div>
+                </dl>
+
+                <div className="flex justify-end">
+                  {/* <span className="inline-flex items-center justify-center rounded-full bg-indigo-100 px-2.5 py-0.5 text-indigo-700">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="-ms-1 me-1.5 size-4"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 010 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 010-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375z"
+                    />
+                  </svg>
+
+                  <p className="whitespace-nowrap text-xs">
+                    2 Discounts Applied
+                  </p>
+                </span> */}
+                </div>
+
+                <div className="flex justify-end">
+                  <a
+                    href="#"
+                    className="block rounded bg-gray-700 px-5 py-3 text-sm text-gray-100 transition hover:bg-gray-600"
+                  >
+                    Checkout
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default CartItems;
