15b9d8f92293b649de1fd02255d43de6c6b3966c
Ashakuur1 <engshakrayare114@gmail.com>
2024-09-07 18:50:51 +0300

    Refactor ProductDetail component to use useCart hook and update product type

--- client/src/components/Products/ProductDetail.tsx
+++ client/src/components/Products/ProductDetail.tsx
@@ -1,89 +1,174 @@
-// import { ProductType } from "@/types/product.t";
-// import { useEffect, useState } from "react";
-// import { useParams } from "react-router-dom";
+import { useCart } from "@/features/useCart";
+import { ProductType } from "@/types/product.t";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
-// const ProductDetail = () => {
-//   const [Product, setProduct] = useState<ProductType | undefined>(undefined);
-//   const { id } = useParams();
+interface ProductCart {
+  id: string | null;
+  name: string;
+  image: string;
+  price: number;
+  sex: string;
+  quantity: number;
+  selectedColor: string;
+  selectedSize: string;
+}
 
-//   const fetchProduct = async () => {
-//     const response = await fetch(`/api/products/${id}`);
-//     const data = await response.json();
-//     console.log(data);
-//     setProduct(data);
-//   };
+const ProductDetail = () => {
+  const [Product, setProduct] = useState<ProductType | undefined>();
 
-//   useEffect(() => {
-//     if (id) {
-//       fetchProduct();
-//     }
-//   }, [id]);
-//   // console.log(Product);
-//   return (
-//     <main className="max-w-[1180px] mx-auto md:px-4 lg:px-0 px-4 py-10">
-//       <div className="flex justify-between gap-12 ">
-//         <div className="flex gap-5 w-[50%] pt-10">
-//           <img src={Product?.image} alt="" className="w-96 h-96" />
-//           <div className="flex flex-col gap-5 pt-1">
-//             <img
-//               src="https://asrv.com/cdn/shop/products/ASRVE-Comm-425-Edit_600x.jpg?v=1673555835"
-//               alt=""
-//               className="w-20"
-//             />
-//             <img
-//               src="https://asrv.com/cdn/shop/products/ASRVE-Comm-425-Edit_600x.jpg?v=1673555835"
-//               alt=""
-//               className="w-20"
-//             />
-//             <img
-//               src="https://asrv.com/cdn/shop/products/ASRVE-Comm-425-Edit_600x.jpg?v=1673555835"
-//               alt=""
-//               className="w-20"
-//             />
-//             <img
-//               src="https://asrv.com/cdn/shop/products/ASRVE-Comm-425-Edit_600x.jpg?v=1673555835"
-//               alt=""
-//               className="w-20"
-//             />
-//           </div>
-//         </div>
-//         <div className="border p-8 w-[50%]">
-//           <div className="flex flex-col gap-2 max-w-md">
-//             <h1>
-//               Name : <strong>{Product?.name}</strong>
-//             </h1>
-//             <span>
-//               Price : <strong>${Product?.price}</strong>{" "}
-//             </span>
-//             <p>
-//               Description : <strong>{Product?.description}</strong>
-//             </p>
-//           </div>
-//           <div className="flex flex-col pt-14 py-4">
-//             <p>Color</p>
-//             <div className="flex gap-3">
-//               {Product?.color.map((color) => (
-//                 <div className="w-10 h-10 bg-blue-400">{color}</div>
-//               ))}
-//             </div>
-//           </div>
-//           <div className="flex flex-col">
-//             <p>Size</p>
-//             <div className="flex gap-3">
-//               {Product?.size.map((size) => (
-//                 <div className="w-10 h-10 bg-blue-400">{size}</div>
-//               ))}
-//             </div>
-//           </div>
-//           <div className="py-4">
-//             <button className="px-4 w-full py-2 bg-black text-white">
-//               Add To Cart
-//             </button>
-//           </div>
-//         </div>
-//       </div>
-//     </main>
-//   );
-// };
+  //   const [selectedColor, setSelectedColor] = useState<string | undefined>();
+  //   const [selectedSize, setSelectedSize] = useState<string | undefined>();
 
-// export default ProductDetail;
+  //   console.log(selectedColor, selectedSize);
+
+  // product detail
+  const [productDetail, setProductDetail] = useState<ProductCart>({
+    id: null,
+    name: "",
+    image: "",
+    price: 0,
+    sex: "",
+    quantity: 1,
+    selectedColor: "",
+    selectedSize: "",
+  });
+
+  // get the id from the url
+  const { id } = useParams();
+
+  // get the cart store object from the cart object
+  const { AddCart, products } = useCart();
+
+  // fetch product information from the server
+  const fetchProduct = async () => {
+    const response = await fetch(`/api/products/${id}`);
+    const data = await response.json();
+    // console.log(data);
+    // const { color: color, size: size } = data;
+    // console.log(rest);
+    setProduct(data);
+    setProductDetail({
+      id: data.id,
+      name: data.name,
+      image: data.image,
+      price: data.price,
+      sex: data.sex,
+      quantity: 1,
+      selectedColor: productDetail.selectedColor,
+      selectedSize: productDetail.selectedSize,
+    });
+  };
+
+  const navigate = useNavigate();
+
+  const handleAddToCart = () => {
+    if (!productDetail.selectedColor || !productDetail.selectedSize) {
+      alert("Please select color and size");
+      return;
+    }
+    AddCart(productDetail);
+    navigate("/cart");
+  };
+
+  useEffect(() => {
+    if (id) {
+      fetchProduct();
+    }
+  }, [id]);
+  console.log(products);
+  return (
+    <main className="max-w-[1180px] mx-auto md:px-4 lg:px-0 px-4 py-10">
+      <div className="flex justify-between gap-12 ">
+        <div className="flex gap-5 w-[50%] pt-10">
+          <img src={Product?.image} alt="" className="w-96 h-96" />
+          <div className="flex flex-col gap-5 pt-1">
+            <img
+              src="https://asrv.com/cdn/shop/products/ASRVE-Comm-425-Edit_600x.jpg?v=1673555835"
+              alt=""
+              className="w-20"
+            />
+            <img
+              src="https://asrv.com/cdn/shop/products/ASRVE-Comm-425-Edit_600x.jpg?v=1673555835"
+              alt=""
+              className="w-20"
+            />
+            <img
+              src="https://asrv.com/cdn/shop/products/ASRVE-Comm-425-Edit_600x.jpg?v=1673555835"
+              alt=""
+              className="w-20"
+            />
+            <img
+              src="https://asrv.com/cdn/shop/products/ASRVE-Comm-425-Edit_600x.jpg?v=1673555835"
+              alt=""
+              className="w-20"
+            />
+          </div>
+        </div>
+        <div className="border p-8 w-[50%]">
+          <div className="flex flex-col gap-5 max-w-md">
+            <h1>
+              Name : <strong>{Product?.name}</strong>
+            </h1>
+            <span>
+              Price : <strong>${Product?.price}</strong>{" "}
+            </span>
+            <p>
+              Description : <strong>{Product?.description}</strong>
+            </p>
+
+            <p>
+              Sex : <strong>{Product?.sex}</strong>
+            </p>
+          </div>
+          <div className="flex flex-col pt-5 pb-4">
+            <p>Color</p>
+            <div className="flex gap-3">
+              {Product?.color.map((color, index) => (
+                <div
+                  onClick={() =>
+                    setProductDetail((prevDetail) => ({
+                      ...prevDetail,
+                      selectedColor: color,
+                    }))
+                  }
+                  key={index}
+                  className="w-10 cursor-pointer h-10 rounded"
+                  style={{ backgroundColor: color }}
+                ></div>
+              ))}
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <p>Size</p>
+            <div className="flex gap-3">
+              {Product?.size.map((size) => (
+                <div
+                  onClick={() =>
+                    setProductDetail((prevDetail) => ({
+                      ...prevDetail,
+                      selectedSize: size,
+                    }))
+                  }
+                  className="w-10 h-10 flex cursor-pointer border items-center text-center justify-center"
+                >
+                  {size}
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="py-4">
+            <button
+              onClick={handleAddToCart}
+              className="px-4 w-full py-2 bg-black text-white"
+            >
+              Add To Cart
+            </button>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default ProductDetail;
