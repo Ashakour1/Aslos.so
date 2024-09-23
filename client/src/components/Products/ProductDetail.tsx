47cf1848f5fcd5a50d0323ae9ca1abdd6c2e303a
Ashakuur1 <engshakrayare114@gmail.com>
2024-09-23 21:44:21 +0300

    Refactor ProductDetail component to use axios for API calls and add loading state

--- client/src/components/Products/ProductDetail.tsx
+++ client/src/components/Products/ProductDetail.tsx
@@ -2,6 +2,7 @@ import { useCart } from "@/features/useCart";
 import { ProductType } from "@/types/product.t";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
 
 interface ProductCart {
   id: string | null;
@@ -16,6 +17,7 @@ interface ProductCart {
 
 const ProductDetail = () => {
   const [Product, setProduct] = useState<ProductType | undefined>();
+  const [loading, setLoading] = useState<boolean>(true); // Loading state
 
   //   const [selectedColor, setSelectedColor] = useState<string | undefined>();
   //   const [selectedSize, setSelectedSize] = useState<string | undefined>();
@@ -42,22 +44,29 @@ const ProductDetail = () => {
 
   // fetch product information from the server
   const fetchProduct = async () => {
-    const response = await fetch(`/api/products/${id}`);
-    const data = await response.json();
-    // console.log(data);
-    // const { color: color, size: size } = data;
-    // console.log(rest);
-    setProduct(data);
-    setProductDetail({
-      id: data.id,
-      name: data.name,
-      image: data.image,
-      price: data.price,
-      sex: data.sex,
-      quantity: 1,
-      selectedColor: productDetail.selectedColor,
-      selectedSize: productDetail.selectedSize,
-    });
+    try {
+      setLoading(true); // Start loading
+
+      const { data } = await axios.get(`/api/products/${id}`);
+      // const data = await response.json();
+      // console.log(data);
+      // const { color: color, size: size } = data;
+      // console.log(rest);
+      setProduct(data);
+      setProductDetail({
+        id: data.id,
+        name: data.name,
+        image: data.image,
+        price: data.price,
+        sex: data.sex,
+        quantity: 1,
+        selectedColor: productDetail.selectedColor,
+        selectedSize: productDetail.selectedSize,
+      });
+    } catch (error) {
+      console.error("Error fetching product:", error);
+    }
+    setLoading(false);
   };
 
   const navigate = useNavigate();
@@ -79,94 +88,100 @@ const ProductDetail = () => {
   console.log(products);
   return (
     <main className="max-w-[1180px] mx-auto md:px-4 lg:px-0 px-4 py-10">
-      <div className="flex justify-between gap-12 ">
-        <div className="flex gap-5 w-[50%] pt-10">
-          <img src={Product?.image} alt="" className="w-96 h-96" />
-          <div className="flex flex-col gap-5 pt-1">
-            <img
-              src="https://asrv.com/cdn/shop/products/ASRVE-Comm-425-Edit_600x.jpg?v=1673555835"
-              alt=""
-              className="w-20"
-            />
-            <img
-              src="https://asrv.com/cdn/shop/products/ASRVE-Comm-425-Edit_600x.jpg?v=1673555835"
-              alt=""
-              className="w-20"
-            />
-            <img
-              src="https://asrv.com/cdn/shop/products/ASRVE-Comm-425-Edit_600x.jpg?v=1673555835"
-              alt=""
-              className="w-20"
-            />
-            <img
-              src="https://asrv.com/cdn/shop/products/ASRVE-Comm-425-Edit_600x.jpg?v=1673555835"
-              alt=""
-              className="w-20"
-            />
-          </div>
+      {loading ? (
+        <div className="flex items-center justify-center h-96">
+          <h1 className="text-xl ">Loading...</h1>
         </div>
-        <div className="border p-8 w-[50%]">
-          <div className="flex flex-col gap-5 max-w-md">
-            <h1>
-              Name : <strong>{Product?.name}</strong>
-            </h1>
-            <span>
-              Price : <strong>${Product?.price}</strong>{" "}
-            </span>
-            <p>
-              Description : <strong>{Product?.description}</strong>
-            </p>
+      ) : (
+        <div className="flex justify-between gap-12 ">
+          <div className="flex gap-5 w-[50%] pt-10">
+            <img src={Product?.image} alt="" className="w-96 h-96" />
+            <div className="flex flex-col gap-5 pt-1">
+              <img
+                src="https://asrv.com/cdn/shop/products/ASRVE-Comm-425-Edit_600x.jpg?v=1673555835"
+                alt=""
+                className="w-20"
+              />
+              <img
+                src="https://asrv.com/cdn/shop/products/ASRVE-Comm-425-Edit_600x.jpg?v=1673555835"
+                alt=""
+                className="w-20"
+              />
+              <img
+                src="https://asrv.com/cdn/shop/products/ASRVE-Comm-425-Edit_600x.jpg?v=1673555835"
+                alt=""
+                className="w-20"
+              />
+              <img
+                src="https://asrv.com/cdn/shop/products/ASRVE-Comm-425-Edit_600x.jpg?v=1673555835"
+                alt=""
+                className="w-20"
+              />
+            </div>
+          </div>
+          <div className="border p-8 w-[50%]">
+            <div className="flex flex-col gap-5 max-w-md">
+              <h1>
+                Name : <strong>{Product?.name}</strong>
+              </h1>
+              <span>
+                Price : <strong>${Product?.price}</strong>{" "}
+              </span>
+              <p>
+                Description : <strong>{Product?.description}</strong>
+              </p>
 
-            <p>
-              Sex : <strong>{Product?.sex}</strong>
-            </p>
-          </div>
-          <div className="flex flex-col pt-5 pb-4">
-            <p>Color</p>
-            <div className="flex gap-3">
-              {Product?.color.map((color, index) => (
-                <div
-                  onClick={() =>
-                    setProductDetail((prevDetail) => ({
-                      ...prevDetail,
-                      selectedColor: color,
-                    }))
-                  }
-                  key={index}
-                  className="w-10 cursor-pointer h-10 rounded"
-                  style={{ backgroundColor: color }}
-                ></div>
-              ))}
+              <p>
+                Sex : <strong>{Product?.sex}</strong>
+              </p>
             </div>
-          </div>
-          <div className="flex flex-col">
-            <p>Size</p>
-            <div className="flex gap-3">
-              {Product?.size.map((size) => (
-                <div
-                  onClick={() =>
-                    setProductDetail((prevDetail) => ({
-                      ...prevDetail,
-                      selectedSize: size,
-                    }))
-                  }
-                  className="w-10 h-10 flex cursor-pointer border items-center text-center justify-center"
-                >
-                  {size}
-                </div>
-              ))}
+            <div className="flex flex-col pt-5 pb-4">
+              <p>Color</p>
+              <div className="flex gap-3">
+                {Product?.color.map((color, index) => (
+                  <div
+                    onClick={() =>
+                      setProductDetail((prevDetail) => ({
+                        ...prevDetail,
+                        selectedColor: color,
+                      }))
+                    }
+                    key={index}
+                    className="w-10 cursor-pointer h-10 rounded"
+                    style={{ backgroundColor: color }}
+                  ></div>
+                ))}
+              </div>
             </div>
-          </div>
-          <div className="py-4">
-            <button
-              onClick={handleAddToCart}
-              className="px-4 w-full py-2 bg-black text-white"
-            >
-              Add To Cart
-            </button>
+            <div className="flex flex-col">
+              <p>Size</p>
+              <div className="flex gap-3">
+                {Product?.size.map((size) => (
+                  <div
+                    onClick={() =>
+                      setProductDetail((prevDetail) => ({
+                        ...prevDetail,
+                        selectedSize: size,
+                      }))
+                    }
+                    className="w-10 h-10 flex cursor-pointer border items-center text-center justify-center"
+                  >
+                    {size}
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="py-4">
+              <button
+                onClick={handleAddToCart}
+                className="px-4 w-full py-2 bg-black text-white"
+              >
+                Add To Cart
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </main>
   );
 };
