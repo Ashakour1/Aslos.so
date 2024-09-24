355a971ac442b90634df05cb52a9e903986d9127
Ashakuur1 <engshakrayare114@gmail.com>
2024-09-24 23:14:12 +0300

    Add conditional rendering for products in Products section

--- client/src/components/Products/products-section.tsx
+++ client/src/components/Products/products-section.tsx
@@ -27,29 +27,32 @@ const Products = () => {
   //     .slice(0, 4);
   // };
   return (
-    <main className="max-w-[1180px] mx-auto md:px-4 lg:px-0 px-4 py-4">
-      <div className="flex items-center justify-between ">
-        <div className="flex flex-col gap-2">
-          <p className="font-medium text-sm">SHOP ALL PRODUCTS</p>
-          <h1 className="text-2xl font-semibold">NEW IN</h1>
+    <>
+      {products.length === 0 ? (
+        <div className="flex items-center justify-center h-96">
+          <h1 className="text-xl font-semibold">No Products Found</h1>
         </div>
-        <Link to="/shop" className="text-sm font-medium">
-          <button className="border border-black px-4 py-2">
-            SHOP ALL PRODUCTS
-          </button>
-        </Link>
-      </div>
-      <div className="grid lg:grid-cols-4   md:grid-cols-3 sm:grid-cols-1 grid-cols-1 gap-2 py-5">
-        {products.map((item, index) => (
-          <Product key={index} product={item} />
-        ))}
-      </div>
-      <div className="flex justify-center">
-        <button className="border border-black px-4 py-2">
-          VIEW ALL PRODUCTS
-        </button>
-      </div>
-    </main>
+      ) : (
+        <main className="max-w-[1180px] mx-auto md:px-4 lg:px-0 px-4 py-4">
+          <div className="flex items-center justify-between ">
+            <div className="flex flex-col gap-2">
+              <p className="font-medium text-sm">SHOP ALL PRODUCTS</p>
+              <h1 className="text-2xl font-semibold">NEW IN</h1>
+            </div>
+            <Link to="/shop" className="text-sm font-medium">
+              <button className="border border-black px-4 py-2">
+                SHOP ALL PRODUCTS
+              </button>
+            </Link>
+          </div>
+          <div className="grid lg:grid-cols-4   md:grid-cols-3 sm:grid-cols-1 grid-cols-1 gap-2 py-5">
+            {products.map((item, index) => (
+              <Product key={index} product={item} />
+            ))}
+          </div>
+        </main>
+      )}
+    </>
   );
 };
 
