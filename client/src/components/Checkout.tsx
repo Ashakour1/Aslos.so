c553a88327036a8d95641ef5ea2ecffae2ed03aa
Ashakuur1 <engshakrayare114@gmail.com>
2024-09-30 20:30:18 +0300

    Refactor Checkout component to integrate CheckoutForm and Payment, implement order submission via WhatsApp, and enhance user experience with toast notifications

--- client/src/components/Checkout.tsx
+++ client/src/components/Checkout.tsx
@@ -1,16 +1,14 @@
 import { useCart } from "@/features/useCart";
-// import { useState } from "react";
 import { TiDeleteOutline } from "react-icons/ti";
 import { Link } from "react-router-dom";
+import CheckoutForm from "./CheckoutForm";
+import { HiOutlineArrowLongLeft } from "react-icons/hi2";
+import Payment from "./Payment";
+import { useCheckout } from "@/features/useCheckout";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const Checkout = () => {
-  // const [shippingDetails, setShippingDetails] = useState({
-  //   email: "",
-  //   phone: "",
-  //   name: "",
-  //   address: "",
-  // });
-
   const { products, totalPrice, tax, totalPriceWithTax, RemoveCart } = useCart(
     (state) => ({
       products: state.products,
@@ -21,20 +19,117 @@ const Checkout = () => {
     })
   );
 
+  const {
+    stage,
+    shippingDetails,
+    paymentMethod,
+    paymentNumber,
+    setShippingDetails,
+    setPaymentNumber,
+    setPaymentMethod,
+    changeStageToInformation,
+  } = useCheckout((state) => ({
+    stage: state.stage,
+    shippingDetails: state.shippingDetails,
+    paymentMethod: state.paymentMethod,
+    paymentNumber: state.paymentNumber,
+    setShippingDetails: state.setShippingDetails,
+    setPaymentNumber: state.setPaymentNumber,
+    setPaymentMethod: state.setPaymentMethod,
+    changeStageToInformation: state.changeStageToInformation,
+  }));
+
   const handleDelete = (id: any) => {
     RemoveCart(id);
   };
 
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
+  const handleFinalSubmit = async () => {
+    // Prepare data to send to backend
+    let orderData = {
+      total: totalPriceWithTax,
+      paymentMethod,
+      paymentNumber,
+      orderItem: products.map((product) => ({
+        product_id: product.id,
+        quantity: product.quantity,
+      })),
+      shipping: {
+        email: shippingDetails.email,
+        phone: shippingDetails.phone,
+        name: shippingDetails.name,
+        address: shippingDetails.address,
+      },
+    };
 
+    const URL = "/api/orders/create";
     try {
-    } catch (error) {}
+      const { data } = await axios.post(URL, orderData);
+      toast.success(data.message);
+      // Clear form fields and cart
+      setPaymentNumber("");
+      setPaymentMethod("");
+      setShippingDetails({
+        email: "",
+        phone: "",
+        name: "",
+        address: "",
+      });
+      products.forEach((product) => {
+        if (product.id !== null) {
+          RemoveCart(product.id);
+        }
+      });
+      changeStageToInformation();
+      const phoneNumber = "+252616590032";
+      // Construct the order details for the message
+      const orderDetails = products
+        .map(
+          (product) =>
+            `Name: ${product.name}, Size: ${product.selectedSize}, Color: ${product.selectedColor}, Price: $${product.price}, Quantity: ${product.quantity}`
+        )
+        .join("\n");
+
+      const totalMessage = `Total Amount: $${totalPrice.toFixed(2)}`; // Ensure formatting for consistency
+      const vatMessage = `VAT (5%): $${tax.toFixed(2)}`; // VAT calculated correctly
+      const totalAmount = `Total Amount (with VAT): $${totalPriceWithTax.toFixed(
+        2
+      )}`; // Total including VAT
+
+      // Include additional shipping and payment information
+      const shippingMessage = `
+        Name: ${shippingDetails.name}
+        Address: ${shippingDetails.address}
+        Phone: ${shippingDetails.phone}
+        Payment Method: ${paymentMethod}
+        Payment Number: ${paymentNumber}
+      `;
+
+      // Encode the message for transmission
+      const encodedMessage = encodeURIComponent(
+        `Order Details:\n${orderDetails}\n${totalMessage}\n${vatMessage}\n\n${totalAmount}\nShipping Details:${shippingMessage}`
+      );
+      const url = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodedMessage}`;
+
+      const newWindow = window.open(url, "_blank");
+      if (newWindow) {
+        newWindow.focus();
+      } else {
+        console.error(
+          "Opening new window failed. Please check your browser settings."
+        );
+      }
+    } catch (error) {
+      toast.error("Error creating order. Please try again."); // User-friendly error message
+      console.error(error);
+    }
+
+    console.log(orderData);
   };
+
   return (
-    <main className="max-w-[1080px] mx-auto md:px-4 lg:px-0 px-4 py-16">
+    <main className="max-w-[1080px] mx-auto md:px-4 lg:px-0 px-4 py-10">
       {products?.length === 0 ? (
-        <div className="text-center  flex flex-col mt-44 justify-center items-center">
+        <div className="text-center flex flex-col mt-44 justify-center items-center">
           <h2 className="text-2xl font-bold text-gray-900">
             No products to checkout.
           </h2>
@@ -47,62 +142,31 @@ const Checkout = () => {
       ) : (
         <div>
           <div className="flex flex-col gap-3">
+            <Link to="#" className="flex items-center text-sm font-medium">
+              <HiOutlineArrowLongLeft className="text-3xl" />
+            </Link>
             <h1 className="text-2xl font-bold">CHECKOUT</h1>
           </div>
           <div className="flex justify-between pt-5">
-            <div className="w-[400px]">
-              <p className="text-lg py-2 font-medium">INFORMATION</p>
-              <h3 className="text-base font-medium">CONTACT INFO</h3>
-              <form action="" onSubmit={handleSubmit}>
-                <div className="py-2">
-                  <input
-                    type="text"
-                    placeholder="Email"
-                    className="border-gray-400 w-full  border py-2 px-4"
-                  />
-                </div>
-                <div className="py-2">
-                  <input
-                    type="text"
-                    placeholder="Phone"
-                    className="border-gray-400 w-full  border py-2 px-4"
-                  />
-                </div>
-                <h1 className="py-2">SHIPPING ADDRESS</h1>
-                <div className="py-2">
-                  <input
-                    type="text"
-                    placeholder="Name"
-                    className="border-gray-400 w-full border py-2 px-4"
-                  />
-                </div>
-
-                <div className="py-2">
-                  <input
-                    type="text"
-                    placeholder="Address"
-                    className="border-gray-400 w-full  border py-2 px-4"
-                  />
-                </div>
-                <button
-                  type="submit"
-                  className="px-4 w-full py-2 mt-2 bg-black text-white "
-                >
-                  Order By Whatsapp
-                </button>
-              </form>
-            </div>
-
+            {stage === "information" ? (
+              <CheckoutForm />
+            ) : (
+              stage === "payment" && <Payment onSubmit={handleFinalSubmit} />
+            )}
             <div className="w-[350px] border p-4">
               <div className="flex justify-between">
                 <h1 className="font-semibold">Your Order List</h1>
-                <p>2</p>
+                <p>{products.length}</p> {/* Dynamic product count */}
               </div>
               {products.map((product) => (
-                <div className="pb-2 pt-5 flex gap-2 w-full">
-                  <img src={product.image} className="w-20 h-20" alt="" />
+                <div key={product.id} className="pb-2 pt-5 flex gap-2 w-full">
+                  <img
+                    src={product.image}
+                    className="w-20 h-20"
+                    alt={product.name}
+                  />
                   <div className="w-full flex flex-col">
-                    <div className="w-full flex  justify-between">
+                    <div className="w-full flex justify-between">
                       <div className="w-full flex flex-col">
                         <h1 className="font-bold text-base">{product.name}</h1>
                         <p className="text-xs pt-1 font-semibold text-gray-500">
@@ -110,7 +174,7 @@ const Checkout = () => {
                         </p>
                       </div>
                       <TiDeleteOutline
-                        className="text-2xl"
+                        className="text-2xl cursor-pointer"
                         onClick={() => handleDelete(product.id)}
                       />
                     </div>
@@ -129,10 +193,6 @@ const Checkout = () => {
                   <dt className="text-xs">Sub total</dt>
                   <dd className="text-xs">${totalPrice}</dd>
                 </div>
-                {/* <div className="flex justify-between py-1">
-                  <p className="text-xs">Delivery</p>
-                  <strong className="text-xs">$1</strong>
-                </div> */}
                 <div className="flex justify-between py-1">
                   <p className="text-xs">VAT 5%</p>
                   <strong className="text-xs">${tax.toFixed(2)}</strong>
@@ -141,7 +201,7 @@ const Checkout = () => {
               <hr />
               <div className="flex justify-between text-xs">
                 <p>Total</p>
-                <strong className="text-xs">${totalPriceWithTax}</strong>{" "}
+                <strong className="text-xs">${totalPriceWithTax}</strong>
               </div>
             </div>
           </div>
