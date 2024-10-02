01a7ebb369fc9a6abc1b9e0258ff9168701df7b2
Ashakuur1 <engshakrayare114@gmail.com>
2024-10-03 01:46:39 +0300

    Add Helmet for document title management in ProductPage

--- Admin/src/pages/ProductPage.tsx
+++ Admin/src/pages/ProductPage.tsx
@@ -2,6 +2,8 @@ import { ProductTable } from "@/components/ProductTable";
 import { Button } from "@/components/ui/button";
 import { useUser } from "@/hooks/useUser";
 import { Link, useNavigate } from "react-router-dom";
+import { Helmet } from "react-helmet";
+
 const ProductPage = () => {
   const { user } = useUser();
 
@@ -11,21 +13,26 @@ const ProductPage = () => {
     navigate("/");
   }
   return (
-    <div className="max-w-[1200px] mx-auto my-10">
-      <div className="flex justify-between my-5 items-center">
-        <div className="flex flex-col gap-2">
-          <h1 className="text-3xl font-semibold">Products</h1>
-          <p className="text-gray-700">
-            Manage your products and view your inventory
-          </p>
+    <div>
+      <Helmet>
+        <title>Products</title>
+      </Helmet>
+      <div className="max-w-[1200px] mx-auto my-10">
+        <div className="flex justify-between my-5 items-center">
+          <div className="flex flex-col gap-2">
+            <h1 className="text-3xl font-semibold">Products</h1>
+            <p className="text-gray-700">
+              Manage your products and view your inventory
+            </p>
+          </div>
+          <div>
+            <Link to="/dashboard/products/add">
+              <Button>Create New Product</Button>
+            </Link>
+          </div>
         </div>
-        <div>
-          <Link to="/dashboard/products/add">
-            <Button>Create New Product</Button>
-          </Link>
-        </div>
+        <ProductTable />
       </div>
-      <ProductTable />
     </div>
   );
 };
