03937d15e7fff39be23d1d47bb64c99e6bd2a1e7
Ashakuur1 <engshakrayare114@gmail.com>
2024-10-03 01:42:15 +0300

    Revamp OurPhilosophy component layout and content for enhanced user engagement and clarity

--- client/src/pages/About/OurPhilosophy.tsx
+++ client/src/pages/About/OurPhilosophy.tsx
@@ -1,68 +1,88 @@
 const OurPhilosophy = () => {
   return (
-    <main className="w-full h-full">
-      <div className="relative flex flex-col items-center justify-center h-[500px]  overflow-hidden">
-        <img
-          src="/banner1.jpg"
-          alt=""
-          className="absolute w-full h-full p-4  inset-0 object-cover"
-        />
-        <div className="relative text-center  text-white max-w-4xl mx-auto px-4 py-8 md:px-8 md:pt-12 ">
-          <h1 className=" text-2xl md:text-7xl font-semibold">
-            OUR PHILOSOPHY
-          </h1>
+    <main className="mx-auto max-w-[1180px] h-full px-4 py-2 sm:px-6 sm:py-4 lg:px-8">
+      <h1 className="text-xs font-semibold  text-gray-900 sm:text-lg pt-8">
+        OUR PHILOSOPHY
+      </h1>
+      <div className="flex flex-col gap-5">
+        <div className=" pt-3">
+          <img
+            src="/banner2.jpg"
+            alt=""
+            className="w-full h-[400px] object-cover py-3"
+          />
+          <div>
+            <p className="text-base text-gray-500">
+              We believe in the power of authenticity and the profound
+              connection between body and mind during physical activity. Our
+              journey began with a heartfelt moment of inspiration from my
+              two-year-old daughter, who affectionately mispronounced my name,
+              Warsame, as "Aslos." This genuine expression encapsulates our
+              commitment to embracing who you are without apologies.
+            </p>
+            <p className="text-base text-gray-500 py-2">
+              Our philosophy is grounded in the understanding that fitness goes
+              beyond physical exertion; it’s a journey of self-discovery. We all
+              experience those extraordinary moments where we become fully
+              absorbed in an activity, losing all self-consciousness and finding
+              deep joy in the process.
+            </p>
+          </div>
+        </div>
+        <div className=" pt-3">
+          <img
+            src="/banner3.png"
+            alt=""
+            className="w-full h-[400px] object-cover py-3"
+          />
+          <div>
+            <p className="text-base text-gray-500">
+              Inspired by these peak experiences, Aslos was founded to integrate
+              the discipline and mindset fostered through fitness into every
+              facet of life. Training is a continuous journey with no endpoint.
+              Each day presents new challenges and opportunities for growth,
+              demanding both mental focus and physical effort. Our most
+              fulfilling workouts occur when we push our limits physically while
+              our minds are fully engaged, losing track of time and becoming
+              fully immersed in the activity.
+            </p>
+            <p className="text-base text-gray-500 py-2">
+              Life is a sequence of pursuits, offering constant challenges to
+              achieve new goals and explore different paths. Rather than viewing
+              challenges as obstacles, we see them as opportunities to cultivate
+              resilience, strength, and endurance for an ever-evolving future.
+              Whether achieving success or facing setbacks, our relentless
+              pursuit means approaching every situation with unwavering
+              intensity and energy, finding our zone and maintaining it.
+            </p>
+          </div>
+        </div>
+        <div className=" pt-3">
+          <img
+            src="/banner1.jpg"
+            alt=""
+            className="w-full h-[400px] object-cover py-3"
+          />
+          <div>
+            <p className="text-base text-gray-500">
+              Aslos is a community of fitness enthusiasts, designers, and
+              creatives united by our shared passion for pushing boundaries.
+              While activewear traditionally supports those striving for
+              championships or external recognition, our primary aim is to
+              empower individuals who relentlessly pursue new challenges,
+              seeking the delicate balance between pressure and capability,
+              where they discover their deepest sense of purpose.
+            </p>
+            <p className="text-base text-gray-500 py-2">
+              Our mission is to empower you to embrace your true self without
+              hesitation, providing the confidence and comfort necessary to
+              achieve new heights. At Aslos, we celebrate individuality and
+              authenticity, helping you unlock your full potential in every
+              aspect of your life.
+            </p>
+          </div>
         </div>
       </div>
-      {/* Our Philosophy content
-       */}
-      <section className="max-w-4xl mx-auto py-5 text-gray-500 lg:px-0 md:px-4  px-5">
-        <div className="flex flex-col items-start justify-start py-8">
-          <p className="text-sm  ">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
-            laboriosam, voluptates, quidem, quas quos quia voluptatem
-            exercitationem quibusdam molestias quae doloremque. Quos, quas
-            voluptates. Quisquam, quae. Quibusdam, quos. Lorem ipsum dolor sit
-            amet consectetur adipisicing elit. Nemo laboriosam, voluptates,
-            quidem, quas quos quia voluptatem exercitationem quibusdam molestias
-            quae doloremque. Quos, quas voluptates. Quisquam, quae. Quibusdam,
-            quos.
-          </p>
-          <img src="/landing.png" alt="" className="w-full h-56 py-4" />
-          <p className="text-sm  ">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
-            laboriosam, voluptates, quidem, quas quos quia voluptatem
-            exercitationem quibusdam molestias quae doloremque. Quos, quas
-            voluptates. Quisquam, quae. Quibusdam, quos. Lorem ipsum dolor sit
-            amet consectetur adipisicing elit. Nemo laboriosam, voluptates,
-            quidem, quas quos quia voluptatem exercitationem quibusdam molestias
-            quae doloremque. Quos, quas voluptates. Quisquam, quae. Quibusdam,
-            quos.
-          </p>
-        </div>
-        <div className="flex flex-col items-start justify-start py-8">
-          <p className="text-sm  ">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
-            laboriosam, voluptates, quidem, quas quos quia voluptatem
-            exercitationem quibusdam molestias quae doloremque. Quos, quas
-            voluptates. Quisquam, quae. Quibusdam, quos. Lorem ipsum dolor sit
-            amet consectetur adipisicing elit. Nemo laboriosam, voluptates,
-            quidem, quas quos quia voluptatem exercitationem quibusdam molestias
-            quae doloremque. Quos, quas voluptates. Quisquam, quae. Quibusdam,
-            quos.
-          </p>
-          <img src="/landing.png" alt="" className="w-full h-56 py-4" />
-          <p className="text-sm  ">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
-            laboriosam, voluptates, quidem, quas quos quia voluptatem
-            exercitationem quibusdam molestias quae doloremque. Quos, quas
-            voluptates. Quisquam, quae. Quibusdam, quos. Lorem ipsum dolor sit
-            amet consectetur adipisicing elit. Nemo laboriosam, voluptates,
-            quidem, quas quos quia voluptatem exercitationem quibusdam molestias
-            quae doloremque. Quos, quas voluptates. Quisquam, quae. Quibusdam,
-            quos.
-          </p>
-        </div>
-      </section>
     </main>
   );
 };
