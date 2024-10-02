b72dbaffce90566e2e481c17f9d58dc4a7aae379
Ashakuur1 <engshakrayare114@gmail.com>
2024-10-03 01:49:29 +0300

    Add Helmet for document title management in ProductFormPage

--- Admin/src/pages/ProductFormPage.tsx
+++ Admin/src/pages/ProductFormPage.tsx
@@ -1,6 +1,7 @@
 import ProductForm from "@/components/ProductForm";
 import { useUser } from "@/hooks/useUser";
 import { useNavigate } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 const ProductFormPage = () => {
   const { user } = useUser();
@@ -12,6 +13,9 @@ const ProductFormPage = () => {
   }
   return (
     <main className="flex min-h-screen items-center justify-center bg-gray-100 py-20 px-4">
+      <Helmet>
+        <title>Product Form</title>
+      </Helmet>
       <ProductForm />
     </main>
   );
